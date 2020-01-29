bde23033e7443d1556dad3af44de9ef3d3179504
Bob Zimmermann <robert.zimmermann@univie.ac.at>
2020-01-29 16:54:36 -0500

    setup.py relintn

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ setup(
     name='moduledev',
     version='0.1',
     packages=['moduledev'],
-    #url='https://bitbucket.org/bobzimmermann/model_selector',
     license='MIT',
-    install_requires=['click','PyYAML','colorama'],
+    install_requires=['click', 'PyYAML', 'colorama'],
     entry_points={
         'console_scripts': ['moduledev=moduledev.cli:mdcli']
     },
@@ -19,7 +18,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
-      ],
+    ],
     author='Bob Zimmermann',
     author_email='robert.paul.zimmermann@gmail.com',
     description='''
